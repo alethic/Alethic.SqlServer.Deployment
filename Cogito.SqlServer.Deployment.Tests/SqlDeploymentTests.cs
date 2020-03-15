0e99e0d819d80654abbf2b5985c8f39eca15a9ac
Jerome Haltom <wasabi@cogito.cx>
2020-03-15 13:20:55 -0500

    Rename some projects. Remove some unused stuff.

--- Cogito.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
+++ Cogito.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
@@ -14,14 +14,14 @@ namespace Cogito.SqlServer.Deployment.Tests
         [TestMethod]
         public void Can_load_example()
         {
-            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Example.xml"));
+            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Test.xml"));
             var d = SqlDeployment.Load(x);
         }
 
         [TestMethod]
         public void Can_compile_example()
         {
-            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Example.xml"));
+            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Test.xml"));
             var d = SqlDeployment.Load(x);
             var p = d.Compile();
         }
@@ -30,7 +30,7 @@ namespace Cogito.SqlServer.Deployment.Tests
         public async Task Can_execute_example()
         {
             using var l = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
-            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Example.xml"));
+            var x = XDocument.Load(typeof(SqlDeploymentTests).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tests.Test.xml"));
             var d = SqlDeployment.Load(x);
             var p = d.Compile();
 
