4016a300b084f0dfb2f1f35504333c5576a1ef3d
Jerome Haltom <wasabi@cogito.cx>
2020-03-15 22:15:41 -0500

    Added separate test cases for localdb.

--- Cogito.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
+++ Cogito.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
@@ -15,6 +15,10 @@ namespace Cogito.SqlServer.Deployment.Tests
 
         public TestContext TestContext { get; set; }
 
+        /// <summary>
+        /// Gets the arguments for the test run.
+        /// </summary>
+        /// <returns></returns>
         Dictionary<string, string> GetArgs()
         {
             return new Dictionary<string, string>()
@@ -24,25 +28,51 @@ namespace Cogito.SqlServer.Deployment.Tests
         }
 
         [TestMethod]
-        public void Can_load_example()
+        public void Can_load_devel_test()
         {
-            var x = XDocument.Load(File.OpenRead("Cogito.SqlServer.Deployment.Tests.Database.xml"));
+            var x = XDocument.Load(File.OpenRead("devel_test.xml"));
             var d = SqlDeployment.Load(x);
         }
 
         [TestMethod]
-        public void Can_compile_example()
+        public void Can_compile_devel_test()
         {
-            var x = XDocument.Load(File.OpenRead("Cogito.SqlServer.Deployment.Tests.Database.xml"));
+            var x = XDocument.Load(File.OpenRead("devel_test.xml"));
             var d = SqlDeployment.Load(x);
             var p = d.Compile(GetArgs());
         }
 
         [TestMethod]
-        public async Task Can_execute_example()
+        public async Task Can_execute_devel_test()
         {
             using var l = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
-            var x = XDocument.Load(File.OpenRead("Cogito.SqlServer.Deployment.Tests.Database.xml"));
+            var x = XDocument.Load(File.OpenRead("devel_test.xml"));
+            var d = SqlDeployment.Load(x);
+            var p = d.Compile(GetArgs());
+
+            await new SqlDeploymentExecutor(p, l.CreateLogger<SqlDeploymentExecutor>()).ExecuteAsync();
+        }
+
+        [TestMethod]
+        public void Can_load_local_test()
+        {
+            var x = XDocument.Load(File.OpenRead("local_test.xml"));
+            var d = SqlDeployment.Load(x);
+        }
+
+        [TestMethod]
+        public void Can_compile_local_test()
+        {
+            var x = XDocument.Load(File.OpenRead("local_test.xml"));
+            var d = SqlDeployment.Load(x);
+            var p = d.Compile(GetArgs());
+        }
+
+        [TestMethod]
+        public async Task Can_execute_local_test()
+        {
+            using var l = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+            var x = XDocument.Load(File.OpenRead("local_test.xml"));
             var d = SqlDeployment.Load(x);
             var p = d.Compile(GetArgs());
 
