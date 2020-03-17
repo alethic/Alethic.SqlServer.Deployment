c07233c93579e7285032808abfb100df441baa33
Jerome Haltom <wasabi@cogito.cx>
2020-03-17 14:16:17 -0500

    Move license code out.

--- Cogito.SqlServer.Deployment.Tool/Program.cs
+++ Cogito.SqlServer.Deployment.Tool/Program.cs
@@ -9,30 +9,17 @@ namespace Cogito.SqlServer.Deployment.Tool
     [Command]
     [Subcommand(typeof(Deploy))]
     [Subcommand(typeof(License))]
-    public class Program
+    public partial class Program
     {
 
-        [Command("license", Description = "displays the license")]
-        public class License
-        {
-
-            public int OnExecute(CommandLineApplication app, IConsole console)
-            {
-                using var l = new StreamReader(typeof(License).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tool.LICENSE"));
-                console.WriteLine(l.ReadToEnd());
-                return 0;
-            }
-
-        }
-
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(@"Cogito SQL Server Deployment Tool");
             Console.WriteLine(@"Copyright 2020 Jerome Haltom");
             Console.WriteLine();
             Console.ResetColor();
-            CommandLineApplication.Execute<Program>(args);
+            return CommandLineApplication.Execute<Program>(args);
         }
 
         /// <summary>
