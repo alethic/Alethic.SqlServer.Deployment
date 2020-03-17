b941ad7cccab167b74c637a3211c24f95af3bc8a
Jerome Haltom <wasabi@cogito.cx>
2020-03-17 14:14:28 -0500

    Clean up command output. Add license. Add debug mode.

--- Cogito.SqlServer.Deployment.Tool/Deploy.cs
+++ Cogito.SqlServer.Deployment.Tool/Deploy.cs
@@ -7,6 +7,7 @@ using System.Xml.Linq;
 
 using McMaster.Extensions.CommandLineUtils;
 
+using Microsoft.Build.Framework;
 using Microsoft.Extensions.Logging;
 
 namespace Cogito.SqlServer.Deployment.Tool
@@ -15,35 +16,42 @@ namespace Cogito.SqlServer.Deployment.Tool
     /// <summary>
     /// Executes a SQL Server Deployment.
     /// </summary>
-    [Command(Name = "deploy", Description = "Execute SQL Server Deployment")]
+    [Command("deploy", Description = "Execute SQL Server Deployment")]
     class Deploy
     {
+
+        /// <summary>
+        /// Gets the parent program.
+        /// </summary>
+        public Program Parent { get; set; }
 
         /// <summary>
         /// Gets or sets the path to the SQL deployment manifest file.
         /// </summary>
         [Argument(0, "manifest", "deployment manifest file")]
+        [Required]
+        [FileExists]
         public string Manifest { get; set; }
 
         /// <summary>
         /// Gets or sets the set of targets to be executed by the deployment.
         /// </summary>
-        [Option("-t | --target", "target name to execute", CommandOptionType.MultipleValue)]
+        [Argument(1, "target(s) to execute")]
         public List<string> Targets { get; set; } = new List<string>();
 
         /// <summary>
         /// Gets or sets the parameter arguments to be passed to the deployment.
         /// </summary>
-        [Option("-a | --argument", "argument to pass to deployment (Name=Value)", CommandOptionType.MultipleValue)]
+        [Option("-a <name>=<value> | --argument <name>=<value>", "argument to pass to deployment (<name>=<value>)", CommandOptionType.MultipleValue)]
         public List<string> Arguments { get; set; } = new List<string>();
 
         /// <summary>
         /// Executes the deployment.
         /// </summary>
         /// <returns></returns>
-        public async Task<int> OnExecute()
+        public async Task<int> OnExecuteAsync(CommandLineApplication app, IConsole console)
         {
-            using var f = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace));
+            using var f = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(Parent.Debug ? LogLevel.Trace : LogLevel.Information));
             var l = f.CreateLogger("SqlDeployment");
 
             var manifest = Path.IsPathFullyQualified(Manifest) == false ? Path.GetFullPath(Manifest, Environment.CurrentDirectory) : Manifest;
--- Cogito.SqlServer.Deployment.Tool/Program.cs
+++ Cogito.SqlServer.Deployment.Tool/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.IO;
+
 using McMaster.Extensions.CommandLineUtils;
 
 namespace Cogito.SqlServer.Deployment.Tool
@@ -6,10 +8,51 @@ namespace Cogito.SqlServer.Deployment.Tool
 
     [Command]
     [Subcommand(typeof(Deploy))]
+    [Subcommand(typeof(License))]
     public class Program
     {
 
-        public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+        [Command("license", Description = "displays the license")]
+        public class License
+        {
+
+            public int OnExecute(CommandLineApplication app, IConsole console)
+            {
+                using var l = new StreamReader(typeof(License).Assembly.GetManifestResourceStream("Cogito.SqlServer.Deployment.Tool.LICENSE"));
+                console.WriteLine(l.ReadToEnd());
+                return 0;
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"Cogito SQL Server Deployment Tool");
+            Console.WriteLine(@"Copyright 2020 Jerome Haltom");
+            Console.WriteLine();
+            Console.ResetColor();
+            CommandLineApplication.Execute<Program>(args);
+        }
+
+        /// <summary>
+        /// Sets whether or not debug level output will be produced.
+        /// </summary>
+        [Option("-d | --debug", Description = "enable debug mode", Inherited = true)]
+        public bool Debug { get; set; } = false;
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="console"></param>
+        /// <returns></returns>
+        public int OnExecute(CommandLineApplication app, IConsole console)
+        {
+            console.Error.Write("You must specify a subcommand.");
+            app.ShowHelp();
+            return 1;
+        }
 
     }
 
