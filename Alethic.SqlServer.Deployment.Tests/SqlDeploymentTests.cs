d37c21765e28de27fa34c8d77d0f25569b8f7ca1
Jerome Haltom <jhaltom@alethic.solutions>
2023-11-25 13:02:31 -0600

    Fix tests.

--- Alethic.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
+++ Alethic.SqlServer.Deployment.Tests/SqlDeploymentTests.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
-using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Alethic.SqlServer.Deployment.Tests
